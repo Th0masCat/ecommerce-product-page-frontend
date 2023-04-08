bb61d44f67507dfe334d813a4aadfe4251b7628f
Th0masCat <catsaredogstoo@gmail.com>
2023-04-08 23:25:45 +0530

    carousel finsih

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,4 +1,3 @@
-import { Carousel } from "@mantine/carousel"
 import "./carousel.css"
 import img1 from "../../images/image-product-1.jpg"
 import img2 from "../../images/image-product-2.jpg"
@@ -8,64 +7,45 @@ import { Image, Flex, Button } from "@mantine/core"
 import { useEffect, useState } from "react"
 
 export default function ImageCarousel() {
-    const [imageNumber, setImageNumber] = useState(1)
-    const totalImages = 4
-
-    const nextSlide = () => {
-        setImageNumber((num) => num + 1)
-    }
-
-    const prevSlide = () => {
-        setImageNumber((num) => num - 1)
-
-    }
+    const images = [img1, img2, img3, img4]
+    const [imageNumber, setImageNumber] = useState(0)
 
     useEffect(() => {
-        if (imageNumber > totalImages) setImageNumber(totalImages)
-        if (imageNumber < 1) setImageNumber(1)
         console.log(imageNumber)
     }, [imageNumber])
+
+    const handleClick = (num) => {
+        switch (num) {
+            case 1:
+                setImageNumber(0)
+                break;
+            case 2:
+                setImageNumber(1)
+                break;
+            case 3:
+                setImageNumber(2)
+                break;
+            case 4:
+                setImageNumber(3)
+                break;
+            default:
+                break;
+        }
+    }
 
     return (
         <Flex direction="column" align="center" justify="center"
         >
-            <Carousel
-                onNextSlide={nextSlide}
-                onPreviousSlide={prevSlide}
-                slideSize="100%"
-                w={"30rem"}
-                slideGap="lg"
-                controlsOffset="xl"
-                controlSize={29}
-                withIndicators
-                initialSlide={2}
-            >
-                <Carousel.Slide>
-                    <Image src={img1} radius={'md'} alt="img1" />
-                </Carousel.Slide>
-                <Carousel.Slide>
-                    <Image src={img2} radius={'md'} alt="img2" />
-                </Carousel.Slide>
-                <Carousel.Slide>
-                    <Image src={img3} radius={'md'} alt="img3" />
-                </Carousel.Slide>
-                <Carousel.Slide>
-                    <Image src={img4} radius={'md'} alt="img4" />
-                </Carousel.Slide>
-            </Carousel>
+            <Image width={'30rem'} src={images[imageNumber]} radius={'md'} alt="img1" />
             <Flex mt="lg" w={"100%"} gap="lg" justify="space-between" align="center">
-                <Button h={"6rem"} w={"6rem"} radius={'md'} p={0} style={{ border: 'none' }}>
-                    <Image width={'100%'} src={img1} radius={'md'} alt="img1" />
-                </Button>
-                <Button h={"6rem"} w={"6rem"} radius={'md'} p={0} style={{ border: 'none' }}>
-                    <Image width={'100%'} src={img2} radius={'md'} alt="img1" />
-                </Button>
-                <Button h={"6rem"} w={"6rem"} radius={'md'} p={0} style={{ border: 'none' }}>
-                    <Image width={'100%'} src={img3} radius={'md'} alt="img1" />
-                </Button>
-                <Button h={"6rem"} w={"6rem"} radius={'md'} p={0} style={{ border: 'none' }}>
-                    <Image width={'100%'} src={img4} radius={'md'} alt="img1" />
-                </Button>
+                {
+                    images.map((image, index) => (
+                        <Button key={image} onClick={() => handleClick(index + 1)} h={"6rem"} w={"6rem"} radius={'md'} p={0} style={{ border: 'none' }}>
+                            <Image width={'100%'} src={image} radius={'md'} alt="img1" />
+                        </Button>
+                    ))
+
+                }
             </Flex>
         </Flex>
 
